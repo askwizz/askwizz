68f93931aa42ce593f4098f03c7938cb69333fcd
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-05-24 13:59:16 +0000

    connect: atlassian: init

---
+++ api/src/api/settings.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, BaseSettings, Field
+
+
+class OAuthConfig(BaseModel):
+    client_id: str
+    client_secret: str
+
+
+class AppSettings(BaseSettings):
+    oauth_atlassian: OAuthConfig = Field(default=...)
+
+    class Config:
+        env_prefix = "api_"
+        env_nested_delimiter = "__"
