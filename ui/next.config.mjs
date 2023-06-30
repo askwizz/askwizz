07a0e8f55d34f617bbbd04a235e6e1201aaeefb7
mDuval1 <maxime.duval@dnascript.co>
2023-06-30 22:32:47 +0200

    feat: make host of backend changeable for local development

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:slug*",
-        destination: "http://api:8000/api/:slug*",
+        destination: `http://${process.env.API_HOST ?? "api"}:8000/api/:slug*`,
       },
     ];
   },
